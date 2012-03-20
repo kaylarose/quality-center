2e758f382162d28704f0857c46b0a6759d84781b
Carl Brasic <carl.brasic@pearson.com>
2012-03-20 12:01:04 -0500

    add some methods, stubbed to point to local xml

--- quality_center.rb
+++ quality_center.rb
@@ -62,45 +62,50 @@ class QualityCenter
     "http://qualitycenter.ic.ncs.com:8080/qcbin/rest/domains/{domain}/projects/{project}/event-logs"
   end
 
-
-end
-
-# get the value of the Name attribute for a field
-def name(field)
-  field.attributes['Name'].value
-end
-
-def users
-  return @users if @users
-  usernames={}
-  doc = Nokogiri::XML.parse(File.read '/home/brasca/git/qc_rest/users.xml.ignore')
-  doc.css('User').each do |user|
-    short = name(user)
-    full  = user.attributes['FullName'].value
-    usernames[ name(user).lowercase ] = full.empty? ? short : full
+  # get the value of the Name attribute for a field
+  def name(field)
+    field.attributes['Name'].value
   end
-  usernames
-end
 
-# get the value of the field, converting things like dates and user names
-def value(field)
-  if DATE_FIELDS.include? (name=name(field))
-    Time.parse(field.text) rescue field.text
-  elsif USER_FIELDS.include? name
-    users[field.text.lowercase]
-  else
-    field.text
+  def users
+    return @users if @users
+    usernames={}
+    doc = Nokogiri::XML.parse(File.read '/home/brasca/git/qc_rest/users.xml.ignore')
+    doc.css('User').each do |user|
+      short = name(user)
+      full  = user.attributes['FullName'].value
+      usernames[ name(user).downcase ] = full.empty? ? short : full
+    end
+    usernames
   end
-end
 
-# convert a single defect xml fragement into a hash
-def defect_to_hash(xml)
-  defect={}
-  xml.css('Field').each do |field|
-    unless (text=field.text).empty?
-      defect[ name(field) ] = value(field)
+  # get the value of the field, converting things like dates and user names
+  def value(field)
+    if DATE_FIELDS.include? (name=name(field))
+      Time.parse(field.text) rescue field.text
+    elsif USER_FIELDS.include? name
+      users[field.text.downcase]
+    else
+      field.text
     end
   end
-  defect
+
+  # convert a single defect xml fragement into a hash
+  def defect_to_hash(xml)
+    defect={}
+    xml.css('Field').each do |field|
+      unless (text=field.text).empty?
+        defect[ name(field) ] = value(field)
+      end
+    end
+    defect
+  end
+
+  def defects
+    dxml = Nokogiri::XML.parse(File.read '/home/brasca/git/qc_rest/defects.xml.ignore')
+    dxml.css('Entity').map{|defect| defect_to_hash(defect)}
+  end
+
+
 end
 
