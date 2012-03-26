d3e88bcd7fcc05f6b1f19024be00a53710dcc956
Carl Brasic <carl.brasic@pearson.com>
2012-03-26 15:35:29 -0500

    simplify slightly

--- test/query_spec.rb
+++ test/query_spec.rb
@@ -4,20 +4,24 @@ class TestQuery < Test::Unit::TestCase
 
   include QualityCenter::RemoteInterface
 
+  def assert_query(expected,actual)
+    assert_equal(expected,actual.query)
+  end
+
   def test_paginate_default
-    assert_equal( Query::DEFAULT[:paging], Query.new.paginate.query)
+    assert_query( Query::DEFAULT[:paging], Query.new.paginate )
   end
 
   def test_paginate_custom
-    assert_equal( {"page-size"=>9, "start-index"=>14}, Query.new.paginate("page-size"=>9, "start-index"=>14).query )
+    assert_query( {"page-size"=>9, "start-index"=>14}, Query.new.paginate("page-size"=>9, "start-index"=>14) )
   end
 
   def test_order_default
-    assert_equal( {"order-by"=>"{id[DESC]}"}, Query.new.order_by(:id).query )
+    assert_query( {"order-by"=>"{id[DESC]}"}, Query.new.order_by(:id) )
   end
   
   def test_order_custom
-    assert_equal( {"order-by"=>"{field[ASC]}"}, Query.new.order_by(:field,'direction'=>'ASC').query )
+    assert_query( {"order-by"=>"{field[ASC]}"}, Query.new.order_by(:field,'direction'=>'ASC') )
   end
 
   def test_order_check_direction
