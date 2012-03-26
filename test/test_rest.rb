9a2d643b99a71f5b585062ca040a553c6c8918a8
Carl Brasic <carl.brasic@pearson.com>
2012-03-26 17:24:35 -0500

    test exceptions, reorg test helpers, new fixture for 404

--- test/test_rest.rb
+++ test/test_rest.rb
@@ -5,7 +5,8 @@ class TestRest < Test::Unit::TestCase
 
   @@good_creds = {user:'valid_user',   password:'valid_password'}
   @@bad_creds  = {user:'invalid_user', password:'invalid_password'}
-  @@match_opts = {:match_requests_on => [:uri,:body]}
+  @@match_opts = {:match_requests_on => [:uri,:body],:record => :new_episodes}
+
 
   def new_rest(creds)
     Rest.new(creds.merge(logger: Logger.new('/dev/null')))
@@ -19,10 +20,17 @@ class TestRest < Test::Unit::TestCase
     VCR.use_cassette('failed_login',@@match_opts){ yield }
   end
 
-  def test_login_success
+  def while_logged_in
     with_valid_credentials do
-      response = new_rest(@@good_creds).login(true)
-      assert_equal(response.code,200)
+      @conn = new_rest(@@good_creds)
+      @login_response = @conn.login(true)
+      yield
+    end
+  end
+
+  def test_login_success
+    while_logged_in do
+      assert_equal(@login_response.code,200)
     end
   end
 
@@ -32,18 +40,24 @@ class TestRest < Test::Unit::TestCase
     end
   end
 
-  def test_login_required
-    with_invalid_credentials do
-      assert_raises(Rest::NotAuthenticated){ new_rest(@@bad_creds).defects }
-    end
-  end
-
-  def test_authentication_check
+  def test_checks_authentication
     with_valid_credentials do
       conn = new_rest(@@good_creds)
       refute conn.authenticated?
       conn.login
       assert conn.authenticated?
+    end
+  end
+
+  def test_exception_without_login
+    with_invalid_credentials do
+      assert_raises(Rest::NotAuthenticated){ new_rest(@@bad_creds).defects }
+    end
+  end
+
+  def test_exception_on_bad_path
+    while_logged_in do
+      assert_raises(Rest::UnrecognizedResponse){ @conn.auth_get '/blah' }
     end
   end
 
