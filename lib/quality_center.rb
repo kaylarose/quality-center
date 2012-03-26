46fc78401405a98c74eea5e9033a391a6942d9d6
Carl Brasic <carl.brasic@pearson.com>
2012-03-26 13:30:29 -0500

    require constants

--- lib/quality_center.rb
+++ lib/quality_center.rb
@@ -1,2 +1,4 @@
+require_relative 'quality_center/constants'
 require_relative 'quality_center/remote_interface'
 require_relative 'quality_center/parser'
+require_relative 'quality_center/defect'
--- lib/quality_center/remote_interface.rb
+++ lib/quality_center/remote_interface.rb
@@ -1,2 +1,3 @@
 require_relative 'remote_interface/rest'
 require_relative 'remote_interface/query'
+require_relative 'constants'
