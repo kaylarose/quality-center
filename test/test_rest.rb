e0251153cd93d9d9ddeed3485cdad8066b211313
Carl Brasic <carl.brasic@pearson.com>
2012-03-26 16:36:01 -0500

    add some helpers

--- test/test_rest.rb
+++ test/test_rest.rb
@@ -11,18 +11,24 @@ class TestRest < Test::Unit::TestCase
     Rest.new(creds.merge(logger: Logger.new('/dev/null')))
   end
 
+  def with_valid_credentials
+    VCR.use_cassette('successful_login',@@match_opts){ yield }
+  end
+
+  def with_invalid_credentials
+    VCR.use_cassette('failed_login',@@match_opts){ yield }
+  end
+
   def test_login_success
-    VCR.use_cassette('successful_login',@@match_opts) do 
+    with_valid_credentials do
       response = new_rest(@@good_creds).login(true)
       assert_equal(response.code,200)
     end
   end
 
   def test_login_failure
-    VCR.use_cassette('failed_login',@@match_opts) do
-      assert_raises(Rest::LoginError) do
-        new_rest(@@bad_creds).login
-      end
+    with_invalid_credentials do
+      assert_raises(Rest::LoginError){ new_rest(@@bad_creds).login }
     end
   end
 
