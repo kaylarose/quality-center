df03407d46d198342907654cb35af6613cb8fcf6
Carl Brasic <carl.brasic@pearson.com>
2012-03-27 14:16:50 -0500

    make relative to project root

--- test/rest_helper.rb
+++ test/rest_helper.rb
@@ -3,7 +3,7 @@ require 'webmock'
 require 'vcr'
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
   c.hook_into :webmock
 end
 
